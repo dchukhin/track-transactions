874518074b96b9717890aff9f14acca9f37dd496
Dmitriy Chukhin <dchukhin@gmail.com>
2019-07-25 16:34:37 -0400

    show transactions on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,22 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <div>Track Transactions</div>
+    <div class="list">
+      <div class="list-row">
+        <span>name</span>
+        <span>date</span>
+        <span>amount</span>
+        <span>description</span>
+        <span>category</span>
+      </div>
+      <div v-for="transaction in transactions" class="list-row">
+        <span>{{ transaction.name }}</span>
+        <span>{{ transaction.date }}</span>
+        <span>{{ transaction.amount }}</span>
+        <span>{{ transaction.description }}</span>
+        <span>{{ transaction.category }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,5 +25,16 @@
 export default {
   name: 'Home',
   components: {},
+  computed: {
+    transactions() {
+      return this.$store.state.transactions;
+    },
+  },
 };
 </script>
+<style scoped lang="scss">
+.list .list-row {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+}
+</style>
