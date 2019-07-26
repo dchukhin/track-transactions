e083d311aa8c11cc43603b396101c69911adcd9f
Dmitriy Chukhin <dchukhin@gmail.com>
2019-07-26 10:01:00 -0400

    use network-first cache strategy for transactions list

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,8 @@
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(
+  'https://my-json-server.typicode.com/dchukhin/track-transactions/transactions',
+  new workbox.strategies.NetworkFirst()
+);
